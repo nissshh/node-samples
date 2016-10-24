646be3cc5c71bb1ad58eaaef7c31ad83641c037e
Nishant Sonar <nishant_sonar@yahoo.com>
2016-10-24 13:02:52 +0530

    Added Some node samples to look at including express.

---
+++ samples/node-server.js
@@ -0,0 +1,21 @@
+var http = require("http");
+var fs = require("fs");
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   var responseString = "I am not replaced!";
+   // Send the response body as "Hello World"
+   
+	fs.readFile('input.txt', function (err, data) {
+		if (err) return console.error(err);
+			console.log(data.toString());
+			responseString = data;
+	});
+   response.end(responseString);
+   console.log("Program Ended");
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
